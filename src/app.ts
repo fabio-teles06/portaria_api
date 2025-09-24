845c556322eaeb10512050d83c8d178300f8cd36
biog4m3 <biog4m3ryt@gmail.com>
2025-09-24 08:20:38 -0300

    Add person routes: implement PersonRoutes with user and admin middleware, and define routes for creating persons and adding users.

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import AuthRoutes from "./routes/authRoutes";
+import PersonRoutes from "./routes/personRoutes";
 
 const app = express();
 app.use(express.json());
 
 app.use("/auth", AuthRoutes);
+app.use("/person", PersonRoutes);
 
 export default app;
---
+++ src/routes/personRoutes.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import PersonController from "../controllers/personController";
+import UserMiddleware from "../middlewares/userMiddleware";
+import AdminMiddleware from "../middlewares/adminMiddleware";
+
+const PersonRoutes = Router();
+
+PersonRoutes.use(UserMiddleware);
+PersonRoutes.use(AdminMiddleware);
+
+PersonRoutes.post("/", PersonController.create);
+PersonRoutes.post("/:id/user", PersonController.addUser);
+
+export default PersonRoutes;
