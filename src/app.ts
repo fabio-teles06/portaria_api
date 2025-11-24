e79bb864b8c21f01eb66a4b4fe449acc404e9ecb
biog4m3 <biog4m3ryt@gmail.com>
2025-11-24 09:17:03 -0300

    Refactor code structure for improved readability and maintainability

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,29 @@
 import express from "express";
+import prisma from "./lib/prisma";
 const app = express();
 app.use(express.json());
 
+app.get("/", async (req, res) => {
+  //Condominio de ID 0 é o condominio padr��o para administradores
+  const condominium = await prisma.condominium.create({
+    data: {
+      id: 0,
+      name: "Condominio Padrão",
+      address: "Endereço Padrão",
+    },
+  });
 
+  const user = await prisma.user.create({
+    data: {
+      name: "Adiministrador",
+      email: "admin@eportaria.com",
+      password: "$2a$14$PXfsCT0bvUttDp2pHXVjD.WoziIq6KGtTiTryHiDDiXEWAg8TQLcO",
+      role: "ADMIN",
+      condominiumId: condominium.id,
+    },
+  });
+
+  res.json({ condominium, user });
+});
 
 export default app;
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,8 @@
-import { PrismaClient } from "@prisma/client"
-const prisma = new PrismaClient()
-export default prisma
+import { PrismaClient } from "../generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+const prisma = new PrismaClient({ adapter });
+export default prisma;
